aeaf86e08953a6a017f1ad3947789a87d997921a
Vito Makhatadze <vitomaxatadze@gmail.com>
2021-08-10 16:54:19 +0400

    Refactor abouts.

--- resources/views/client/pages/about/index.blade.php
+++ resources/views/client/pages/about/index.blade.php
@@ -9,23 +9,7 @@
     <div class="about_us_page wrapper">
         <div class="main_title">{{ $aboutPage->title }}</div>
         <div class="container">
-            <div class="img">
-                <img src="/img/other/1.png" alt=""/>
-            </div>
-            <div class="title">@lang("client.about_company_history")</div>
-            <div class="paragraph dark">
-                {!! $aboutPage->description !!}
-            </div>
-            <div class="img">
-                <img src="/img/other/1.png" alt=""/>
-            </div>
-            {{--            <div class="paragraph dark">--}}
-            {{--                შემთხვევითად გენერირებული ტექსტი ეხმარება დიზაინერებს და ტიპოგრაფიული--}}
-            {{--                ნაწარმის შემქმნელებს, რეალურთან მაქსიმალურად მიახლოებული შაბლონი--}}
-            {{--                წარუდგინონ შემფასებელს. ხშირადაა შემთხვევა, როდესაც დიზაინის--}}
-            {{--                შესრულებისას საჩვენებელია, თუ როგორი იქნება ტექსტის ბლოკი. სწორედ ასეთ--}}
-            {{--                დროს არის მოსახერხებელი ამ გენერატორით შექმნილი ტექსტის გამოყენება,--}}
-            {{--            </div>--}}
+            {!! $aboutPage->description !!}
         </div>
         <div class="bottom">
             <button class="arrow" id="prev_about">
@@ -38,7 +22,8 @@
             <div class="bottom_flex bottom_about_slide flex">
                 @foreach($certificates as $certificate)
                     <div class="item">
-                        <img class="img" src="{{ url($certificate->file ? $certificate->file->file_url : '') }}" alt="{{ $certificate->title }}">
+                        <img class="img" src="{{ url($certificate->file ? $certificate->file->file_url : '') }}"
+                             alt="{{ $certificate->title }}">
                         <div class="paragraph">{{ $certificate->title }}</div>
                     </div>
                 @endforeach
